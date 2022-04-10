b8f28a39e8fc675afa77d7a206d45443d98be35a
Polis Minus <polisminus2247@tuta.io>
2022-04-10 15:22:18 +0400

    Started frontend with tkinter example

---
+++ frontend/main.py
@@ -0,0 +1,10 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("Ashborne")
+window.geometry("810x477")
+
+label = tkinter.Label(window, text="Hello, World!")
+label.pack()
+
+window.mainloop()
