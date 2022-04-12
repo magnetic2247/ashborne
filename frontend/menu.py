077098bd72a3e6ef5d22f59ee84b489125700438
Polis Minus <polisminus2247@tuta.io>
2022-04-12 23:51:10 +0400

    Main Menu Prototype

--- frontend/menu.py
+++ frontend/menu.py
@@ -11,16 +11,15 @@ ws.title("Ashborne")
 ws.geometry(f"{window_width}x{window_height}")
 ws.resizable(0,0)
 
-
 # Background Image
-#l = Label(ws, image=background)
-l = Canvas(ws, width=window_width, height=window_height)
-l.pack(expand=YES, fill=BOTH)
-background = PhotoImage(file="assets/Menu_BG.png")
-l.create_image(0, 0, image=background, anchor=NW)
+canvas = Canvas(ws, width=window_width, height=window_height)
+canvas.pack(expand=YES, fill=BOTH)
+background = PhotoImage(file="assets/MenuBackground.png")
+canvas.create_image(0, 0, image=background, anchor=NW)
 
-# Button
-play_button = Button(l, text="Play", width=34, bd=0)
-play_button.place(y=100)
+# Play Button
+play_button = Button(canvas, text="Play", width=34, bd=0)
+play_button.place(x=275, y=200)
 
+# Main Loop
 ws.mainloop()
