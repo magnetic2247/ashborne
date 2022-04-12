e8ce10c6f32897f218a81f0b1e9b99ec3b0d7da1
Polis Minus <polisminus2247@tuta.io>
2022-04-13 00:35:46 +0400

    Added spinbox for round selection

--- frontend/menu.py
+++ frontend/menu.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python
 from tkinter import *
-import tkinter.font
+import subprocess
 
 # Window Size
 window_width = 810
@@ -17,9 +18,21 @@ canvas.pack(expand=YES, fill=BOTH)
 background = PhotoImage(file="assets/MenuBackground.png")
 canvas.create_image(0, 0, image=background, anchor=NW)
 
+# Spinbox for Round Count
+spinny_boi = Spinbox(ws, from_=5, to=100, width=35)
+spinny_boi.place(x=276, y=232)
+
 # Play Button
 play_button = Button(canvas, text="Play", width=34, bd=0)
 play_button.place(x=275, y=200)
 
+
+# Button Clicked Event
+def button_clicked(event):
+    global play_button
+    subprocess.run(["python", "frontend/game.py", spinny_boi.get()]) # This is hacky at best and should never be used. I'm tired.
+
+# Bind Events
+play_button.bind('<Button-1>', button_clicked)
 # Main Loop
 ws.mainloop()
